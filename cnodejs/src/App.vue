abb9d76d9b9e347ca125b72aa7f4d6c9aeb32bd7
GaoKu <2172393524@qq.com>
2021-07-23 11:00:28 +0800

    cnodejs

---
+++ cnodejs/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+body {
+  background-color: #e1e1e1;
+  padding: 0 0.05rem!important;
+}
+</style>
---
+++ cnodejs/src/views/Topic.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="box">
+      <div v-html="$store.state.des.content"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
+<style>
+#box{
+    font-size: .14rem;
+}
+</style>
